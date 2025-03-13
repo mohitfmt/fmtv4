1b7a266389e291bc8e5fdd2a7d663b6ef7345edf
mohit5783 <mohit5783@gmail.com>
2025-03-13 16:11:33 +0800

    changed stg-cms to cms env NEXT_PUBLIC_CMS_URL not working

--- pages/api/websub-callback.ts
+++ pages/api/websub-callback.ts
@@ -361,7 +361,7 @@ export default async function handler(
 
       // Get current domains
       const wpDomain =
-        process.env.NEXT_PUBLIC_CMS_URL || "staging-cms.freemalaysiatoday.com";
+        process.env.NEXT_PUBLIC_CMS_URL || "cms.freemalaysiatoday.com";
       const frontendDomain =
         process.env.NEXT_PUBLIC_DOMAIN || "dev-v4.freemalaysiatoday.com";
 
