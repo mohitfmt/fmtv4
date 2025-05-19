89111938a961e562091bf5a08857ca2e59625ded
Raj Chhalotre <raj@uraimastermindz.com>
2025-05-19 20:23:15 +0530

    Disable section data api

--- pages/api/fetch-section-data.ts
+++ pages/api/fetch-section-data.ts
@@ -1,29 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getCategoryNews } from "@/lib/gql-queries/get-category-news";
-import { ParsedUrlQuery } from "querystring";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Destructure query parameters safely
-  const { category, limit } = req.query as ParsedUrlQuery;
-
-  const parsedLimit = Number(limit);
-
-  // Validate that category is provided
-  if (!category) {
-    return res
-      .status(400)
-      .json({ error: "Missing required category parameter" });
-  }
-
-  try {
-    const posts = await getCategoryNews(category as string, parsedLimit, false);
-
-    return res.status(200).json(posts);
-  } catch (error) {
-    console.error("error in api", error);
-    return res.status(500).json({ error: "Failed to fetch posts" });
-  }
+  // pages/api/fetch-section-data.ts
+  return res.status(410).json({ error: "This endpoint is deprecated." });
 }
